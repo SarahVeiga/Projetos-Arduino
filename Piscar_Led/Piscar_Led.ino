8655934281dad02b9e34be502255fae73ac6fcab
Sarah Veiga <sarahveiga55@gmail.com>
2024-08-05 15:11:01 -0300

    alteração

--- Piscar_Led/Piscar_Led.ino
+++ Piscar_Led/Piscar_Led.ino
@@ -1,11 +1,16 @@
 void setup() {
+  //porta configurada como saída
   pinMode (7, OUTPUT);
+  //Programa que envia sinal alto para a saída 7, 
+  //Aguarda 1 segundo e envia sinal baixo para saída 7
 
 }
 
 void loop() {
+  //envia sinal alto
   digitalWrite(7,HIGH);
   delay (1000);
+  // envia sinal baixo
   digitalWrite(7,LOW);
   delay(1000);
 
